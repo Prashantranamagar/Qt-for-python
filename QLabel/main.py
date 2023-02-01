5adf3a49bf2b187bfa4f91a79b4bc1a3260b14f6
Prashant Rana Magar <ranamagar.prashant@gamil.com>
2023-02-01 10:26:06 +0545

    QLabel

---
+++ QLabel/main.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QApplication
+from widget import Widget
+import sys
+
+
+app = QApplication(sys.argv)
+
+widget = Widget()
+widget.show()
+
+app.exec()
--- QLabel/widget.py
+++ QLabel/widget.py
@@ -1,2 +1,18 @@
-from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QWidget, QLabel,
+    from PySide6.QtGui import QPixmap
+    from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout 
+
+
+    class Widget(QWidget):
+        def  __init__(self):
+            super().__init__()
+
+            self.setWindowTitle("QLabel Image Demo")
+            image_label =QLabel()
+            image_label.setPixmap(QPixmap("images/abc.jpg"))
+
+            layout =QVBoxLayout()
+            layout.addWidget(image_label)
+
+            self.setLayout(layout)
+
+
